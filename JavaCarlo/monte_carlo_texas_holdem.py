cdf01a5e79316bf5aba1ab73294d86c13c808d7f
framirez64 <franciscoramirez@unomaha.edu>
2024-04-08 11:51:49 -0500

    idk

--- JavaCarlo/monte_carlo_texas_holdem.py
+++ JavaCarlo/monte_carlo_texas_holdem.py
@@ -13,7 +13,6 @@ def simulate(hand, table, players):
     deck = random.sample(cards,len(cards)) #shuffle the deck
     hand = hand[:]
     table = table[:]
-
     full = table + hand
     deck = list(filter(lambda x: x not in full, deck))
 
